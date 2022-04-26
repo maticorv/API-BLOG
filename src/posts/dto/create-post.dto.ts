430c111ac5f9721330bea62d6fa42691f0afe7b9
Matias Corvalan <matias.corvalan24@gmail.com>
2022-04-25 22:53:44 -0300

    validaciones agregadas

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,10 @@
+import { ArrayNotEmpty, IsArray, IsNotEmpty, IsString } from 'class-validator';
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
   paragraphs: string[];
 }
